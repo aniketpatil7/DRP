31299d60b2d0d8110a90da084b4b11857172ab34
SankalpHub <sankalpt0202@gmail.com>
2025-03-03 00:36:27 +0530

    intial setup

---
+++ saps.cpp
@@ -0,0 +1,21 @@
+
+#define IN1 6
+#define IN2 7
+#define IN3 8
+#define IN4 9
+#define ENA 5
+#define ENB 10
+
+
+void setup() {
+
+    pinMode(IN1, OUTPUT);
+    pinMode(IN2, OUTPUT);
+    pinMode(IN3, OUTPUT);
+    pinMode(IN4, OUTPUT);
+    pinMode(ENA, OUTPUT);
+    pinMode(ENB, OUTPUT);
+}
+void loop(){
+    
+}
