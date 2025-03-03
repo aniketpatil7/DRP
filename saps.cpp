c6267ff5c7691c41a394a2f3904315553e84601a
SankalpHub <sankalpt0202@gmail.com>
2025-03-03 17:49:02 +0530

    Added Forward and Backward motion

--- saps.cpp
+++ saps.cpp
@@ -22,11 +22,21 @@ void loop(){
 
 
 void moveForward() {
-    
+    digitalWrite(IN1, HIGH);
+    digitalWrite(IN2, LOW);
+    digitalWrite(IN3, HIGH);
+    digitalWrite(IN4, LOW);
+    analogWrite(ENA, 150);
+    analogWrite(ENB, 150);
 }
 
 void moveBackward() {
-    
+    digitalWrite(IN1, LOW);
+    digitalWrite(IN2, HIGH);
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, HIGH);
+    analogWrite(ENA, 150);
+    analogWrite(ENB, 150);
 }
 
 void turnLeft() {
